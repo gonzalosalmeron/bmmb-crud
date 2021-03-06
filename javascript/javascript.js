d65cac97f0dcd769a237165bd775c442c0857c97
Gonzalo Salmerón Robles <gonzxlosalmeron@gmail.com>
2021-03-06 18:17:50 +0100

    Add files via upload
    login form upload

---
+++ javascript/javascript.js
@@ -0,0 +1,10 @@
+/************** DARK MODE  ******************/
+function ocultar(id) {
+    var elemento = document.getElementById(id);
+    elemento.style.display = "none";
+}
+
+function mostrar(id) {
+    var elemento = document.getElementById(id);
+    elemento.style.display = "inline";
+}
